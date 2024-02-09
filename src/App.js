e5812c76949389fed31615bc93e5cf231a31a708
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-09 18:42:59 +0300

    create react router dom and other folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import "./App.css";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+    {
+      path: "deneme",
+      element: <div>deneme ekranı</div>,
+    },
+  ]);
+
+  createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+  );
+
   return (
     <div>
       hello
