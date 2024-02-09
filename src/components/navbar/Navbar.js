7a428f22e81f334972bc9bc64c27b26a796bf81e
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-09 19:59:38 +0300

    create edite navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import RightNavbar from "./navbarItems/RightNavbar";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center my-5">
       <LeftNavbar />
       <RightNavbar />
     </div>
--- src/components/navbar/navbarItems/RightNavbar.js
+++ src/components/navbar/navbarItems/RightNavbar.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { FaRegHeart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import { BsBasket } from "react-icons/bs";
 
 const RightNavbar = () => {
-  return <div>RightNavbar</div>;
+  return (
+    <div className="flex items-center gap-8">
+      <div className="flex items-center border p-3 rounded-full bg-gray-200">
+        <input
+          className="bg-gray-200 outline-none"
+          type="text"
+          placeholder="Arama yapınız..."
+        />
+        <IoSearch size={28} />
+      </div>
+      <FaRegHeart size={26} />
+
+      <div className="relative">
+        <div className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center">
+          3
+        </div>
+        <BsBasket size={26} />
+      </div>
+    </div>
+  );
 };
 
 export default RightNavbar;
