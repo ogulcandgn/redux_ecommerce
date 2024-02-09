ae33e1e3cadc1fa042718450c3e574c7712ab14d
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-10 00:10:26 +0300

    create slider classes

--- src/components/home/SliderComp.js
+++ src/components/home/SliderComp.js
@@ -1,34 +1,60 @@
 import React from "react";
 import Slider from "react-slick";
+import shoes1 from "../../assets/shoes-1.jpg";
+import shoes2 from "../../assets/shoes-2.jpg";
+import shoes3 from "../../assets/shoes-3.jpg";
 
 const SliderComp = () => {
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
+    autoplay: true,
+    autoplaySpeed: 1500,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
     <div>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+        <div className="!flex items-center justify-between mt-10 bg-gray-100">
+          <div className="p-5">
+            <div className="text-5xl font-bold">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div className="text-lg mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              tempora.
+            </div>
+            <div className="!text-bold text-orange-800 text-xl">İncele</div>
+          </div>
+          <img src={shoes1} alt="" className="h-auto max-w-xl " />
         </div>
-        <div>
-          <h3>2</h3>
+        <div className="!flex items-center justify-between mt-10 bg-gray-100">
+          <div className="p-5">
+            <div className="text-5xl font-bold">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div className="text-lg mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              tempora.
+            </div>
+            <div className="!text-bold text-orange-800 text-xl">İncele</div>
+          </div>
+          <img src={shoes2} alt="" className="h-auto max-w-xl " />
         </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
+        <div className="!flex items-center justify-between mt-10 bg-gray-100">
+          <div className="p-5">
+            <div className="text-5xl font-bold">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div className="text-lg mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              tempora.
+            </div>
+            <div className="!text-bold text-orange-800 text-xl">İncele</div>
+          </div>
+          <img src={shoes3} alt="" className="h-auto max-w-xl " />
         </div>
       </Slider>
     </div>
--- src/components/navbar/navbarItems/LeftNavbar.js
+++ src/components/navbar/navbarItems/LeftNavbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LeftNavbar = () => {
-  return <div className="text-6xl">ShopZone</div>;
+  return <div className="text-6xl">Shop Zone</div>;
 };
 
 export default LeftNavbar;
