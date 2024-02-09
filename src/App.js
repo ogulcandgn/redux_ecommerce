0f0b5e12f6bf1a6fb2886aa3a55bf1e9c2183183
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-09 18:19:24 +0300

    create tailwind package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      hello
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
