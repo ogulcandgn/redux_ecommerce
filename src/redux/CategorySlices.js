407f36bac3099f10883b044e54873ed1b40f8e0c
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-11 21:41:57 +0300

    export category slices

--- src/redux/CategorySlices.js
+++ src/redux/CategorySlices.js
@@ -4,14 +4,22 @@ const initialState = {
   categories: [],
 };
 
-const getCategories = createAsyncThunk("category", async () => {
+export const getCategories = createAsyncThunk("category", async () => {
   const responce = await fetch("https://fakestoreapi.com/products");
+  const data = responce.json();
+  return data;
 });
 
 const CategorySlice = createSlice({
   name: "categories",
   initialState,
-  reducers,
+  reducers: {},
   //api'den veri çekeceğimiz için extraReducers yapısını kullanıyoruz
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.fulfilled, (state, action) => {
+      state.categories = action.payload;
+    });
+  },
 });
+
+export default CategorySlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CategorySlices from "./CategorySlices";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { CategorySlices },
 });
