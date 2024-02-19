03f22397ccb9b4eae4de30185c6cee9345334d20
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-19 20:59:59 +0300

    create fetchFromLocalStorage and storeInLocalStorage func

--- src/redux/CartSlice.js
+++ src/redux/CartSlice.js
@@ -1,8 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const fetchFromLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
+//*dışarıdan gelen dataları localstorage'e kaydeder
+const storeInLocalStorage = (data) => {
+  localStorage.setItem("cart", JSON.stringify(data));
+};
+
+const initialState = {
+  cart: fetchFromLocalStorage(),
+  itemCount: 0,
+  totalAmount: 0,
+};
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    addToCart: (state, action) => {},
+  },
 });
