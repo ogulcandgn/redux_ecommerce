ae16d7d3ac769c34d35f2452e8eaebf6586a4b92
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-20 19:51:29 +0300

    CartComp edit tailwind

--- src/components/cart/CartComp.js
+++ src/components/cart/CartComp.js
@@ -2,9 +2,24 @@ import React from "react";
 
 const CartComp = ({ cart }) => {
   return (
-    <div className="my-10 p-2">
-      <img className="w-[150px] h-[150px]" src={cart?.image} alt="" />
-      <div></div>
+    <div className="my-10 p-2 flex items-center justify-between">
+      <img
+        className="w-[150px] h-[150px] border p-3 shadow-md"
+        src={cart?.image}
+        alt=""
+      />
+      <div>
+        <div>
+          {cart?.title}
+          {cart?.description}
+        </div>
+      </div>
+      <div>
+        {cart?.price} TL ({cart?.quantity})
+      </div>
+      <div className="bg-red-500 text-white cursor:pointer p-3 text-xl rounded-md flex items-center justify-center ">
+        Delete Product
+      </div>
     </div>
   );
 };
